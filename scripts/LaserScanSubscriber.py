58a3bc01e24b527ea5d21d73006ca411539cb90b
Evanchuan <kofour23348@gmail.com>
2021-09-02 10:37:40 +0800

    2021 Sep 2, Need Modify LaserScan

--- scripts/LaserScanSubscriber.py
+++ scripts/LaserScanSubscriber.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan
-from std_srvs import SetBoolRequest, SetBoolResponse
+from std_srvs.srv import SetBool, SetBoolRequest
+
+triggerPauseArmService = None
 
 def callback(data):
-    triggerPauseArmService = rospy.ServiceProxy('/pause_arm_service', SetBool)
+    global triggerPauseArmService
     req = SetBoolRequest()
     laser_range = data.ranges
     res = any(i <= 1 for i in laser_range)
     if res == True:
         req.data = True
-        #rospy.loginfo("something in")
+        rospy.loginfo("something in")
     else:
         req.data = False
-        #rospy.loginfo("safe")
+        rospy.loginfo("safe")
+    
     try: 
+        print "ck1"
         res = triggerPauseArmService(req)
+        print "ck2"
         return
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
     
 
 def listener():
+    global triggerPauseArmService
     rospy.init_node('LaserScanSubscriber', anonymous=True)
     rospy.Subscriber("/octopoda/amr0/front_scan", LaserScan, callback)
+    triggerPauseArmService = rospy.ServiceProxy('/pause_arm_service', SetBool)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
